c0b8540b5fb1e050b9d83f6160d826d44b87189d
RudraJoshi0006 <rudrajoshi3653@gmail.com>
2025-08-21 15:40:05 +0530

    Add files via upload

---
+++ DIVISION.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+float c;
+printf("Enter the two Numbers");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%d/%d=%f",a,b,c);
+//a=9/5.0;
+//printf("%.2f",a);
+}
